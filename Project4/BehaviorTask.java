6cf66e3342fc7573b88d35810c2a544bac316df4
Aram Sodhi <93051510+aramsodhi@users.noreply.github.com>
2026-03-01 19:12:00 -0800

    Create  BehaviorTask.java

---
+++ Project4/BehaviorTask.java
@@ -0,0 +1,15 @@
+public class BehaviorTask extends Task {
+    private final World world;
+    private final ImageLibrary imageLibrary;
+
+    public BehaviorTask(Entity entity, World world, ImageLibrary imageLibrary) {
+        super(entity);
+        this.world = world;
+        this.imageLibrary = imageLibrary;
+    }
+
+    public void execute(EventScheduler scheduler) {
+        Entity entity = getEntity();
+        entity.executeBehavior(world, imageLibrary, scheduler);
+    }
+}
